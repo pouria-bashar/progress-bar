3b03e2bfb0ed07d5e7aa7973b142471f98b0d5a3
Pouria Bashardoust <pouria.bashardoust@justice.gov.nsw.au>
2017-08-08 20:18:12 +1000

    Added progress bar

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ProgressBar.css';
+
+export const colors = {
+  alert: '#e74c3c',
+  progress: '#2980b9'
+};
+
+const getWidth = (percentage) => {
+  if(percentage < 0) return 0;
+  else if(percentage > 100) return 100;
+  return percentage;
+};
+
+const getBackground = (percentage) => percentage > 100 ? colors.alert: colors.progress;
+
+const ProgressBar = ({ percentage }) => (
+  <div className={styles.progressBar}>
+    <span>%{percentage}</span>
+    <div style={{ width: `${getWidth(percentage)}%` , background: getBackground(percentage) }} />
+  </div>
+);
+
+ProgressBar.propTypes = {
+  percentage: PropTypes.number,
+};
+
+ProgressBar.defaultProps = {
+  percentage: 0,
+};
+
+export default ProgressBar;
---
+++ tests/components/ProgressBar-test.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import chai , { expect } from 'chai';
+import { shallow } from 'enzyme';
+import ProgressBar, { colors } from '../../src/components/ProgressBar';
+
+describe('<ProgressBar />', () => {
+  
+  it('renders', function() {
+    const wrapper = shallow(<ProgressBar />);
+    expect(wrapper).to.have.length(1);
+  });
+
+  it('default percentage is zero', function() {
+    const wrapper = shallow(<ProgressBar />);
+    expect(wrapper.text()).to.equal('%0');
+  });
+
+  it('percentage is correctly shown', function() {
+    const wrapper = shallow(<ProgressBar percentage={20} />);
+    expect(wrapper.text()).to.equal('%20');
+  });
+
+  it('progress bar has correct width', () => {
+    const alerPercentages = [101, 120];
+    const negativePercentages = [-1, -20];
+    const naturalPercentages = [0, 10, 99, 100];
+
+    alerPercentages.map((percentage) => {
+      const wrapper = shallow(<ProgressBar percentage={percentage} />).childAt(1);
+      const width = wrapper.props().style.width;
+      expect(width).to.equal('100%');
+    });
+
+    negativePercentages.map((percentage) => {
+      const wrapper = shallow(<ProgressBar percentage={percentage} />).childAt(1);
+      const width = wrapper.props().style.width;
+      expect(width).to.equal('0%');
+    });
+
+    naturalPercentages.map((percentage) => {
+      const wrapper = shallow(<ProgressBar percentage={percentage} />).childAt(1);
+      const width = wrapper.props().style.width;
+      expect(width).to.equal(`${percentage}%`);
+    });
+  });
+
+  it('progress bar has correct background color', () => {
+    const alertCases = [101, 120];
+    const progressCases = [-1, 0, 10, 99, 100];
+
+    progressCases.map(percentage => {
+      const wrapper = shallow(<ProgressBar percentage={percentage} />).childAt(1);
+      const { background } = wrapper.props().style;
+      expect(background).to.equal(colors.progress);
+    });
+
+    alertCases.map(percentage => {
+      const wrapper = shallow(<ProgressBar percentage={percentage} />).childAt(1);
+      const { background } = wrapper.props().style;
+      expect(background).to.equal(colors.alert);
+    });
+  });
+})
