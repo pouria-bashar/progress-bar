4c6d4a3298a2160ce03f62c8a16ed44413512af2
Pouria Bashardoust <pouria.bashardoust@justice.gov.nsw.au>
2017-08-08 19:46:23 +1000

    Initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+const { resolve } = require('path');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      use: ['react-hot-loader', 'babel-loader'],
+      include: path.join(__dirname, 'src')
+    },
+    {
+       test: /\.css$/,
+       loaders: [
+         'style-loader',
+         'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[local]___[hash:base64:5]',
+       ]
+     }]
+  }
+};
